4e1128b213e5a747f1ea801c2d2619df76c1b09b
adam  bonjoc <adamkbonjocdpi@gmail.com>
2023-04-12 23:12:26 +0000

    added dynamic route I went to views and change the route for the erb and cleaned up

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,26 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    my_id = self.id
+
+    matching_movies = Movie.where({ :director_id => my_id })
+
+    sorted_movies = matching_movies.order({ :year => :asc })
+
+    return sorted_movies
+  end
+
+  # Three 1-N associations:
+  # - Director => Movie
+  # - Movie => Character
+  # - Actor => Character
+
+  # Try define:
+  #   - Director#filmography done
+  #   - Movie#director done
+  #   - Movie#characters done
+  #   - Character#movie done
+  #   - Actor#characters done
+  #   - Character#actor
 end
